bbeacc313280b01c7b1ed1f3d2e53017dfe10a10
Aakash Patel <aakashdp@umich.edu>
2023-12-09 17:21:21 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+#! /bin/bash
+
+# install openmp stuff
 sudo apt-get install libomp-dev gcc-offload-nvptx
 ln -s /usr/local/cuda-11.8/lib64/libcudart.so /usr/local/lib/   # make symlink to libcudart from lib directory
 
-cmake -S llvm -B build -DLLVM_ENABLE_PROJECTS="clang;compiler-rt" -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_LINKER=gold -DLLVM_ENABLE_RUNTIMES="openmp" -DLLVM_TARGETS_TO_BUILD="NVPTX"
+# build llvm with openmp
+git --depth=1 clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build && cd build
+cmake ../llvm/ -DCMAKE_BUILD_TYPE=Release -G Ninja -DLIBOMPTARGET_DEVICE_ARCHITECTURES=sm_75 -DLLVM_TARGETS_TO_BUILD="X86;NVPTX" -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_ENABLE_RUNTIMES="openmp"
+ninja install

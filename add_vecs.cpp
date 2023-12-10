339e4d6cee40a220ee4a3d73be5f554a74ef17db
Aakash Patel <aakashdp@umich.edu>
2023-12-09 23:15:52 -0500

    updated add_vecs

--- add_vecs.cpp
+++ add_vecs.cpp
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <omp.h>
 
-# define N 1500000000
+#define N 200000000
 
-int main() {
-  float *x = (float*) malloc(N * sizeof(float));
-  float *y = (float*) malloc(N * sizeof(float));
-  float a = 1;
+int main()
+{
+  float *a   = (float *)malloc(sizeof(float) * N);
+  float *b   = (float *)malloc(sizeof(float) * N);
+  float *c   = (float *)malloc(sizeof(float) * N);
 
-  double total_size = 2 * sizeof(float) * N + sizeof(float);
+  double total_size = 3 * sizeof(float) * N;
   printf("Total Size: %f GB\n", (float)((total_size/1024.0)/1024.0/1024.0));
 
-  for (long i = 0; i < N; ++i) {
-    y[i] = a * x[i] + y[i];
-  }
+  double time = -omp_get_wtime();
 
-  free(x);
-  free(y);
+    // fill arrays
+    for (int i = 0; i < N; i++){
+      a[i] = (float)i;
+      b[i] = 2.0 * (float)i;
+    }
+
+    // add vectors
+    for (int i=0; i<N; i++){
+      c[i] = a[i] + b[i];
+    }
+
+  time +=  omp_get_wtime();
+
+  free(a);
+  free(b);
+
+  printf("Time: %.3fs\n", time);
+
+  return 0;
 }
--- add_vecs_offload.cpp
+++ add_vecs_offload.cpp
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <omp.h>
 
-# define N 1500000000
+#define N 200000000
 
-int main() {
-  float *x = (float*) malloc(N * sizeof(float));
-  float *y = (float*) malloc(N * sizeof(float));
-  float a = 1;
+int main()
+{
+  float *a   = (float *)malloc(sizeof(float) * N);
+  float *b   = (float *)malloc(sizeof(float) * N);
+  float *c   = (float *)malloc(sizeof(float) * N);
 
-  double total_size = 2 * sizeof(float) * N + sizeof(float);
+  double total_size = 3 * sizeof(float) * N;
   printf("Total Size: %f GB\n", (float)((total_size/1024.0)/1024.0/1024.0));
 
-  #pragma omp target data map(to: x[0:N])
-  #pragma omp target data map(tofrom: y[0:N])
+  double time = -omp_get_wtime();
+
+  #pragma omp target data map(to:a[0:N], b[0:N], c[0:N])
   {
-    #pragma omp target
-    #pragma omp teams
-    #pragma omp distribute parallel for
-    {
-      for (long i = 0; i < N; ++i) {
-        y[i] = a * x[i] + y[i];
-      }
+    #pragma omp target teams distribute parallel for 
+    for (int i = 0; i < N; i++){
+      a[i] = (float)i;
+      b[i] = 2.0 * (float)i;
+    }
+
+    // add two vectors
+    #pragma omp target teams distribute parallel for
+    for (int i=0; i<N; i++){
+      c[i] = a[i] + b[i];
     }
   }
-  free(x);
-  free(y);
+
+  time +=  omp_get_wtime();
+
+  free(a);
+  free(b);
+
+  printf("Time: %.3fs\n", time);
+
+  return 0;
 }
